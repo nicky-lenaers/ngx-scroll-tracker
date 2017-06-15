c2b1ffaf78f00238b9ba8dce73d150cff953d968
Nicky Lenaers <nicky@nite-dev.nl>
2017-06-15 11:59:37 +0200

    Moved Module Declaration

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,32 @@
-export { ScrollTrackerModule } from './scroll-tracker.module';
-export { ScrollTrackerEventData } from './scroll-tracker.service';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ScrollTrackerDirective } from './scroll-tracker.directive';
+import { ScrollTrackerService } from './scroll-tracker.service';
+
+@NgModule({
+	imports: [
+		CommonModule
+	],
+	declarations: [
+		ScrollTrackerDirective
+	],
+	exports: [
+		ScrollTrackerDirective
+	]
+})
+export class ScrollTrackerModule {
+
+	/**
+	 * Guaranteed singletons for provided Services across App.
+	 *
+	 * @return 				An Angular Module with Providers
+	 */
+	static forRoot(): ModuleWithProviders {
+		return {
+			ngModule: ScrollTrackerModule,
+			providers: [
+				ScrollTrackerService
+			]
+		}
+	}
+}
